1b0837726e2a3167a1f380537760e880dc042420
Lasselarsen4 <10478730@ucn.dk>
2023-05-18 10:53:34 +0200

    Instansvariabler til Order.java - Mangler muligvis noget

---
+++ src/model/Order.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Order {
+	private int orderNo;
+	private LocalDate orderDate;
+	private LocalDate deliveryDate;
+	private double totalCost;
+	private LocalDate payDate;
+	private boolean finished;
+
+}
