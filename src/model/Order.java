81f26e1ad03444ae8635f45131e26dd84a2cc527
Lasselarsen4 <10478730@ucn.dk>
2023-05-21 01:42:12 +0200

    Skrevet videre på Order klassen.
    Skal lige fatte hvordan man skriver resten. Endelig skriv mere på den hvis i forstår - den er kommenteret :)

--- src/model/Order.java
+++ src/model/Order.java
@@ -7,11 +7,46 @@ import java.time.LocalDate;
  *
  */
 public class Order {
-	private int orderNo;
+	private Member m;
+	private int orderNum;
+	private static int orderNumCount = 1;
 	private LocalDate orderDate;
 	private LocalDate deliveryDate;
 	private double totalCost;
 	private LocalDate payDate;
 	private boolean finished;
+	// Mangler OrderLine (List eller ArrayList)
+	
+	/**
+	 *  Constructor
+	 */
+	public Order()
+	{
+		this.orderNum = orderNumCount;
+		orderNumCount++;
+		orderDate = LocalDate.now();
+		payDate = LocalDate.now();
+		finished = false;
+	}
+	/**
+	 * @return Member her kaldt m.
+	 */
+	public Member getM()
+	{
+		return m;
+	}
+	public void setM(Member m)
+	{
+		this.m = m;
+	}
+	
+	public int getOrderNum()
+	{
+		return orderNum;
+	}
+	public void setOrderNum(int orderNum)
+	{
+		this.orderNum = orderNum;
+	}
 
 }
