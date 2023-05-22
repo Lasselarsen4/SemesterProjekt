7ea6bf412d7eb5a02ff9611a6db6643cf3fc1ba8
Lasselarsen4 <10478730@ucn.dk>
2023-05-22 15:21:50 +0200

    Update MemberContainer.java

--- src/model/MemberContainer.java
+++ src/model/MemberContainer.java
@@ -5,7 +5,8 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-public class MemberContainer {
+public class MemberContainer
+{
 	
 	private static MemberContainer instance;
 	private List<Member> members;
@@ -16,7 +17,8 @@ public class MemberContainer {
 	 * at der kun kan oprettes én instans af MemberContainer.
 	 * Constructor kan kun kaldes via getInstance-metoden.
 	 */
-	private MemberContainer() {
+	private MemberContainer() 
+	{
 		members = new ArrayList<>();
 		memberComparator = (m1, m2) -> m1.getEmail().compareTo(m2.getEmail());
 	}
@@ -24,8 +26,10 @@ public class MemberContainer {
 	/**
 	 * @return instansen af MemberContainer
 	 */
-	public static MemberContainer getInstance() {
-		if (instance == null) {
+	public static MemberContainer getInstance() 
+	{
+		if (instance == null) 
+		{
 			instance = new MemberContainer();
 		}
 		return instance;
@@ -35,8 +39,10 @@ public class MemberContainer {
 	 * Tilføjer et medlem til containeren.
 	 * @param member medlemmet, der skal tilføjes
 	 */
-	public void addMember(Member member) {
-		if (member != null) {
+	public void addMember(Member member)
+	{
+		if (member != null)
+		{
 			members.add(member);
 			sortMembers();
 		}
@@ -47,13 +53,15 @@ public class MemberContainer {
 	 * @param email e-mailadressen, der skal søges efter
 	 * @return det medlem, der matcher e-mailadressen, eller null hvis intet match findes
 	 */
-	public Member findMemberByEmail(String email) {
+	public Member findMemberByEmail(String email)
+	{
 		sortMembers();
 		Member result = null;
 		try {
 			int index = Collections.binarySearch(members, new Member(0, null, email, null, null), memberComparator);
 			result = members.get(index);
-		} catch (IndexOutOfBoundsException e) {
+		} catch (IndexOutOfBoundsException e)
+		{
 			return result;
 		}
 		return result;
@@ -62,14 +70,16 @@ public class MemberContainer {
 	/**
 	 * Sorterer medlemmerne i containeren.
 	 */
-	private void sortMembers() {
+	private void sortMembers()
+	{
 		Collections.sort(members, memberComparator);
 	}
 	
 	/**
 	 * @return en liste med alle medlemmer i containeren
 	 */
-	public List<Member> getAllMembers() {
+	public List<Member> getAllMembers()
+	{
 		return members;
 	}
 	
